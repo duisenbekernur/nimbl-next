90f2f6f1e177dccfe350409c4f5efe89df597d8c
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-01-16 17:53:36 +0600

    xcxc

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -5,7 +5,19 @@ import avatar3 from '../../assets/avatar3.png';
 import like from '../../assets/like.svg';
 import dislike from '../../assets/dislike.svg';
 import bored from '../../assets/bored.png';
+import user1 from '../../assets/user1.png';
+import user2 from '../../assets/user2.png';
+import user3 from '../../assets/user3.png';
+import user4 from '../../assets/user4.png';
+import user5 from '../../assets/user5.png';
+import user6 from '../../assets/user6.png';
+import user7 from '../../assets/user7.png';
+import user8 from '../../assets/user8.png';
+import user9 from '../../assets/user9.png';
+import logo from '../../assets/logo.png';
+import arrowDown from '../../assets/arrowDown.svg';
 import Image from 'next/image';
+
 const Community = () => {
   return (
     <div className={styles.community_main}>
@@ -167,10 +179,66 @@ const Community = () => {
       </div>
       <div className={styles.community_chat}>
         <div className={`${styles.community_chat_users}`}>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user1} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user2} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user3} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user4} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user5} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user6} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user7} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user8} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+          <div className={`${styles.community_chat_user}`}>
+            <Image className={`${styles.img}`} src={user9} alt=""/>
+            <div className={`${styles.line}`}></div>
+          </div>
+        </div>
+        <div className={`${styles.community_chat_stat}`}>
+          <div className={`${styles.community_chat_stat_logo}`}>
+            <Image className={`${styles.img}`} src={logo} alt=""/>
+            <div className={`${styles.text}`}>
+              <h1 className={`${styles.title}`}>Bankless</h1>
+              <h3 className={`${styles.details}`}>Community Size: <span>4.2k</span></h3>
+            </div>
+          </div>
+          <div className={`${styles.community_chat_stat_info}`}>
+            <div className={`${styles.stat_block}`}>Floor Price: <span>20 NMBL</span></div>
+            <div className={`${styles.stat_block}`}>Volume: <span>134 NMBL</span></div>
+          </div>
+          <div className={`${styles.community_chat_stat_dropdown}`}>
+            <div className={`${styles.stat_text}`}>
+              <h1>GaryVee #1</h1>
+              <h2>32 online</h2>
+            </div>
+            <Image className={`${styles.img}`} src={arrowDown} alt=""/>
+          </div>
+        </div>
+        <div className={`${styles.community_chat_messages}`}>
           
         </div>
-        <div className={`${styles.community_chat_stat}`}></div>
-        <div className={`${styles.community_chat_messages}`}></div>
       </div>
     </div>
   );
