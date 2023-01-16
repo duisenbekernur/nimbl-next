43341c2f67b8f40056f5ca8a5366e3453d7ae11c
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-01-16 15:25:30 +0600

    Merge branch 'Mikhail' of https://github.com/duisenbekernur/nimbl-next

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,5 +1,24 @@
+import styles from './Community.module.css';
+
 const Community = () => {
-  return <div>Community</div>;
+  return (
+    <div className={styles.community_main}>
+      <div className={styles.community_feed}>
+        <div className={styles.community_feed_title}>
+          <h1>Community Feed</h1>
+        </div>
+        <div className={`${styles.community_item}`}>
+          <div className={`${styles.community_item_userdata}`}>
+            <div className={`${styles.community_item_avatar}`}>
+              <img src="" alt="avatar"/>  
+            </div>    
+            <div className={`${styles.community_item_details}`}></div>
+          </div>    
+        </div>
+      </div>
+      <div className={styles.community_chat}></div>
+    </div>
+  );
 };
 
 export default Community;
