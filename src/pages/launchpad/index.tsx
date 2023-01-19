8ba586266709201f4edd29df83c7e3f603d9ee9b
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-01-19 13:11:22 +0600

    /

---
+++ src/pages/launchpad/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Launchpad.module.css';
+
+const Launchpad = () => {
+  return (
+    <div className={`${styles.launchpad}`}>
+        <div className={`${styles.launchpad_navigation}`}>
+            
+        </div>
+        <div className={`${styles.launchpad_cards}`}>
+
+        </div>
+    </div>
+  );
+}
+
+export default Launchpad;
