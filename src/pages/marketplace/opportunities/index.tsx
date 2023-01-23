8a06a276fa3fb75ece39c76a16817703d14fe3a4
Adiretto <a4912520@gmail.com>
2023-01-23 12:15:09 +0600

    added dropdown for opportunities page

--- src/pages/marketplace/opportunities/index.tsx
+++ src/pages/marketplace/opportunities/index.tsx
@@ -1,12 +1,17 @@
-
 import OpportunityList from '@/components/OpportunityList'
-
+import RecomendationDropdown from '@/components/RecomendationDropdown'
+import styles from './Opportunities.module.scss'
 const Opportunities = () => {
     return (
-        <>
-
+        <main className={styles.container}>
+            <article className={styles.mainContainer}>
+                <RecomendationDropdown />
+                <RecomendationDropdown />
+                <RecomendationDropdown />
+                <RecomendationDropdown />
+            </article>
             <OpportunityList />
-        </>
+        </main>
     )
 }
 
