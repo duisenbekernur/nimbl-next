7bf1f443f125e5c265a1bf8a34db34b3327e2f27
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-01-23 17:14:43 +0600

    launchpad navigation in process

--- src/pages/launchpad/index.tsx
+++ src/pages/launchpad/index.tsx
@@ -1,40 +1,85 @@
 import React from 'react';
 import styles from './Launchpad.module.css';
-import Image from 'next/image'
+import Image from 'next/image';
 import live from '../../assets/launchpad_live.svg';
 import committed from '../../assets/launchpad_committed.svg';
 import mints from '../../assets/launchpad_mints.svg';
 import Rankings from '../../components/Rankings/index';
-import LaunchpadCard from  '../../components/LaunchpadCard/index';
+import LaunchpadCard from '../../components/LaunchpadCard/index';
+import Slider from '../../components/Slider/index';
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
 
 const Launchpad = () => {
-  return (
-    <div className={`${styles.launchpad}`}>
-        <Rankings/>
-        <div className={`${styles.launchpad_navigation}`}>
-            <div className={`${styles.navigation_buttons}`}>
-                <div className={`${styles.button}`}>
-                    <Image alt="" src={live} className={`${styles.button_img}`}/>
-                    <h1 className={`${styles.button_title}`}>Live mints</h1>
+    const swiper = new Swiper('.swiper', {
+        direction: 'vertical',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+    return (
+        <div className={`${styles.launchpad}`}>
+            <Rankings />
+            <div className={`${styles.launchpad_navigation}`}>
+                <div className={`${styles.navigation_buttons}`}>
+                    <div className={`${styles.button}`}>
+                        <Image
+                            alt=""
+                            src={live}
+                            className={`${styles.button_img}`}
+                        />
+                        <h1 className={`${styles.button_title}`}>Live mints</h1>
+                    </div>
+                    <div className={`${styles.button}`}>
+                        <Image
+                            alt=""
+                            src={committed}
+                            className={`${styles.button_img}`}
+                        />
+                        <h1 className={`${styles.button_title}`}>
+                            Finished mints
+                        </h1>
+                    </div>
+                    <div className={`${styles.button}`}>
+                        <Image
+                            alt=""
+                            src={mints}
+                            className={`${styles.button_img}`}
+                        />
+                        <h1 className={`${styles.button_title}`}>
+                            Upcoming mints
+                        </h1>
+                    </div>
                 </div>
-                <div className={`${styles.button}`}>
-                    <Image alt="" src={committed} className={`${styles.button_img}`}/>
-                    <h1 className={`${styles.button_title}`}>Finished mints</h1>
+                <div className={`${styles.navigation_pagination}`}>
+                    <Slider />
                 </div>
-                <div className={`${styles.button}`}>
-                    <Image alt="" src={mints} className={`${styles.button_img}`}/>
-                    <h1 className={`${styles.button_title}`}>Upcoming mints</h1>
+            </div>
+            <div className={`${styles.launchpad_cards}`}>
+                <div className="swiper">
+                    <div className="swiper-wrapper">
+                        <div className="swiper-slide"><LaunchpadCard/></div>
+                        <div className="swiper-slide"><LaunchpadCard/></div>
+                        <div className="swiper-slide"><LaunchpadCard/></div>
+                        <div className="swiper-slide"><LaunchpadCard/></div>
+                    </div>
+                    <div className="swiper-pagination"></div>
+
+                    <div className="swiper-button-prev"></div>
+                    <div className="swiper-button-next"></div>
+
                 </div>
             </div>
         </div>
-        <div className={`${styles.launchpad_cards}`}>
-            <LaunchpadCard/>
-            <LaunchpadCard/>
-            <LaunchpadCard/>
-            <LaunchpadCard/>
-        </div>
-    </div>
-  );
+    )
 }
 
-export default Launchpad;
+export default Launchpad
