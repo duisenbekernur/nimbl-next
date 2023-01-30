dab7f0cf5502407412abaa70818ef286fb25dfc4
Adiretto <a4912520@gmail.com>
2023-01-30 11:09:03 +0600

    C:/Program Files/Git/

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,19 +1,18 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from 'react'
 
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)){
-    const [value, setValue] = useState<T>(() =>{
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+    const [value, setValue] = useState<T>(() => {
         const jsonValue = localStorage.getItem(key)
         if (jsonValue != null) return JSON.parse(jsonValue)
 
-        if(typeof initialValue === "function"){
+        if (typeof initialValue === 'function') {
             return (initialValue as () => T)()
         } else {
             return initialValue
         }
     })
 
-
-    useEffect(() =>{
+    useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [key, value])
 
