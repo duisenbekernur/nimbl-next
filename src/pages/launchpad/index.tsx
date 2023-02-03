c69e0d1cf417ad18a759442c43bd96931e195a95
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-02-03 14:31:01 +0600

    Revert "reverting"
    This reverts commit 7bf1f443f125e5c265a1bf8a34db34b3327e2f27.

--- src/pages/launchpad/index.tsx
+++ src/pages/launchpad/index.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useRef } from 'react'
 import styles from './Launchpad.module.css'
 import Rankings from '../../components/Rankings/index'
@@ -111,8 +112,45 @@ const Launchpad = () => {
         <div className={`${styles.launchpad_cards}`}>
 
 >>>>>>> parent of e9f0e55 (almost there)
+=======
+import React from 'react';
+import styles from './Launchpad.module.css';
+import Image from 'next/image'
+import live from '../../assets/launchpad_live.svg';
+import committed from '../../assets/launchpad_committed.svg';
+import mints from '../../assets/launchpad_mints.svg';
+import Rankings from '../../components/Rankings/index';
+import LaunchpadCard from  '../../components/LaunchpadCard/index';
+
+const Launchpad = () => {
+  return (
+    <div className={`${styles.launchpad}`}>
+        <Rankings/>
+        <div className={`${styles.launchpad_navigation}`}>
+            <div className={`${styles.navigation_buttons}`}>
+                <div className={`${styles.button}`}>
+                    <Image alt="" src={live} className={`${styles.button_img}`}/>
+                    <h1 className={`${styles.button_title}`}>Live mints</h1>
+                </div>
+                <div className={`${styles.button}`}>
+                    <Image alt="" src={committed} className={`${styles.button_img}`}/>
+                    <h1 className={`${styles.button_title}`}>Finished mints</h1>
+                </div>
+                <div className={`${styles.button}`}>
+                    <Image alt="" src={mints} className={`${styles.button_img}`}/>
+                    <h1 className={`${styles.button_title}`}>Upcoming mints</h1>
+                </div>
+            </div>
+>>>>>>> parent of 7bf1f44 (launchpad navigation in process)
         </div>
-    )
+        <div className={`${styles.launchpad_cards}`}>
+            <LaunchpadCard/>
+            <LaunchpadCard/>
+            <LaunchpadCard/>
+            <LaunchpadCard/>
+        </div>
+    </div>
+  );
 }
 
-export default Launchpad
+export default Launchpad;
