453228d81c6074f14bd5516dfb3161bee9126780
Ернур <Eroxa2580@mail.ru>
2023-02-06 15:23:10 +0600

    conflicts fixed

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,29 +2,19 @@ import { configureStore, Store } from '@reduxjs/toolkit'
 import navbarSlice from './features/navbar/navbarSlice'
 import filter from './features/rankings-filter/filter'
 import videos from './features/videos/videosSlice'
-<<<<<<< HEAD
 import transitions from './features/transitions/transitions'
-=======
 import communityFeed from './features/communityFeed/communityFeedSlice'
 import communityChat from './features/communityChat/communityChatSlice'
-
->>>>>>> develop
 
 const store = configureStore({
     reducer: {
         navbar: navbarSlice,
-<<<<<<< HEAD
-        filter: filter,
-        videos,
-        transitions,
-    },
-=======
         filter:filter,
         videos,
+        transitions,
         communityFeed,
         communityChat
     }
->>>>>>> develop
 })
 
 export type RootState = ReturnType<typeof store.getState>
