c17caa9bc3963023cf95b0aff70426fbf6ec1530
Ернур <76120333+duisenbekernur@users.noreply.github.com>
2023-02-09 23:29:20 +0600

    Revert "BREAKING CHANGE: new Main Page"

--- src/components/VideoCard/index.tsx
+++ src/components/VideoCard/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './VideoCard.module.scss'
+import previewImg from '../../assets/video/preview.svg'
 import eyeImg from '../../assets/icons/eye.svg'
 import clockImg from '../../assets/icons/clock.svg'
-import {FC, useState} from 'react'
-import {videoCardType} from '@/types'
+import channelImg from '../../assets/icons/channel-logo.svg'
+import { FC, useRef, useState } from 'react'
+import { videoCardType } from '@/types'
 import Link from 'next/link'
-import useSound from 'use-sound'
+import useSound from 'use-sound';
+
 
 const VideoCard: FC<videoCardType> = ({
     // id,
@@ -16,40 +19,71 @@ const VideoCard: FC<videoCardType> = ({
     poster,
 }) => {
     const [isShow, setIsShow] = useState(false)
+    const videoRef = useRef(null)
     const [videoSound, setVideoSound] = useState(false)
     const soundUrl = '/sounds/ui-click.mp3'
 
-    const [play, {stop}] = useSound(soundUrl, {volume: 5})
+    const [play, { stop }] = useSound(
+        soundUrl,
+        { volume: 5 }
+    );
 
     return (
-        <div className={styles.card}>
-            <div className={styles.card_preview}>
-                <div className={styles.img_wrap}>
-                    <Image src={poster} fill alt="preview" />
+        <div
+            className={styles.card}
+            ref={videoRef}
+            onMouseEnter={() => {
+                setIsShow(true)
+                play()
+            }}
+
+            onMouseLeave={() => {
+                setIsShow(false)
+                stop()
+            }}
+        >
+            <div className={styles.card__preview}>
+                {!isShow ? (
+                    <Image
+                        src={poster}
+                        width={300}
+                        height={100}
+                        alt="preview"
+                    />
+                ) : (
+                    <video
+                        autoPlay={true}
+                        src="./alb_numbers001_1080p_24fps.mp4"
+                    />
+                )}
+                <div className={styles.card__preview__bottom}>
+                    <Link
+                        href="/channel/1"
+                        className={styles.card__preview__bottom_title}
+                        title='Go to the channel'
+                    >
+                        <Image src={channelImg} alt="logo" />
+                        <p>{channel}</p>
+                    </Link>
+
+                    <div className={styles.card__preview__bottom_info}>
+                        <h5>Floor Price:</h5>
+                        <span>{price} NMBL</span>
+                    </div>
                 </div>
             </div>
 
-            <div className={styles.card_header}>
-                <Link href="/channel/1" className={styles.channel_title} title="Go to the channel">
-                    <Image src={avatar} width={30} height={30} alt="logo" />
-                    <p>{channel}</p>
-                </Link>
+            <div className={styles.card__info}>
+                <h3>
+                    {title.length > 40 ? `${title.substring(0, 40)}...` : title}
+                </h3>
 
-                <div className={styles.channel_price}>
-                    <span>Floor Price:</span>
-                    <span>{price} NMBL</span>
-                </div>
-            </div>
-
-            <div className={styles.card_info}>
-                <p className={styles.video_title}>{title}</p>
-
-                <div className={styles.video_details}>
-                    <div className={styles.details_item}>
+                <div className={styles.card__info_bottom}>
+                    <div className={styles.views}>
                         <Image src={eyeImg} alt="views" />
                         <p>100 345</p>
                     </div>
-                    <div className={styles.details_item}>
+                    <div className={styles.date}>
                         <Image src={clockImg} alt="date" />
                         <p>2 days ago</p>
                     </div>
