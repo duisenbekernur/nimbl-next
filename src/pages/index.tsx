3c3d09dcfb5c5e8ddcc67ba82b9adc4bcfa585e5
Ернур <Eroxa2580@mail.ru>
2023-02-13 15:33:25 +0600

    fix: main page trending videos scroll fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,18 @@ import VideoCard from '@/components/VideoCard'
 import withAuthorization from '@/HOC/Authorization/Authorization'
 import {useRouter} from 'next/router'
 import {UseShoppingCart} from '@/context/AuthContext'
-import {useEffect, useRef, useState, WheelEvent} from 'react'
+import {useEffect, useRef} from 'react'
 import {useSelector} from 'react-redux'
 import {RootState} from '@/store/store'
 import Layout from '@/components/Layout'
-import Flicking from '@egjs/react-flicking'
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+import 'swiper/css'
 
 function Home() {
     const router = useRouter()
     const {isAuth} = UseShoppingCart()
     const videos = useSelector((state: RootState) => state.videos.videos)
-    const recomendationsRef = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
         if (!isAuth) {
@@ -38,19 +39,18 @@ function Home() {
                     <Sidebar />
                 </div>
                 <div className={`${styles.content} ${'col-8'}`}>
-                    <Flicking moveType="freeScroll" bound={true}>
-                        <div className={styles.dropdowns}>
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                            <RecomendationDropdown />
-                        </div>
-                    </Flicking>
+                    <Swiper
+                        slidesPerView={4}
+                        spaceBetween={20}
+                        centeredSlides={false}
+                        className={['mySwiper', styles.dropdowns].join(' ')}>
+                        {[...new Array(15)].map((item) => (
+                            <SwiperSlide>
+                                <RecomendationDropdown />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+
                     <div className={styles.content_videos}>
                         {videos?.map((item, index) => (
                             <VideoCard key={index} {...item} />
