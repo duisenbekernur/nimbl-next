4aaf311a7bf98ac603c3eb56487b1f7154a97b49
Eraly <81900112+Chambo015@users.noreply.github.com>
2023-02-14 19:42:58 +0600

    fix: some things

--- src/components/RecomendationDropdown/index.tsx
+++ src/components/RecomendationDropdown/index.tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
 import styles from './RecomendationDropdown.module.scss'
-import img from '@/assets/dropdown/eth.png'
+
 import plus from '../../assets/icons/plus.svg'
+
+
 
 const RecomendationDropdown = (props:any) => {
     return (
         <div className={styles.main}>
             <div className={styles.background}>
-                <Image src={img} alt="trending" />
+                <Image src={props.img} 
+                style={{objectFit: 'cover'}} alt="trending" />
                     <h3>Trending Videos</h3>
-                <div className={styles.plus}>
+                {/* <div className={styles.plus}>
                     <Image src={plus} alt="plus" />
-                </div>
+                </div> */}
             </div>
         </div>
     )
