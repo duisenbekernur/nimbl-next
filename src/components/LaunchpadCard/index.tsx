e28dce33a7919b284791328367d3d25bf17248a5
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-03-04 23:22:54 +0600

    feat: launchpad started

--- src/components/LaunchpadCard/index.tsx
+++ src/components/LaunchpadCard/index.tsx
@@ -1,52 +1,30 @@
-import React from 'react';
-import styles from '../../pages/launchpad/Launchpad.module.css';
-import Image from 'next/image';
-import card_play from '../../assets/launchpad_card_play.svg';
-import com from '../../assets/launchpad_com.svg';
-import border_bottom from '../../assets/launchpad_corner_bottom.svg';
-import border_top from '../../assets/launchpad_corner_top.svg';
+import React from 'react'
+import styles from './LaunchpadCard.module.scss'
+import Image from 'next/image'
+import card_play from '../../assets/play_button.svg'
+import border_bottom from '../../assets/launchpad_corner_bottom.svg'
+import border_top from '../../assets/launchpad_corner_top.svg'
+import Sprite from '../Sprite/index'
 
 const LaunchpadCard = () => {
-  return (
-    <div className={`${styles.launchpad_card}`}>
-                <div className={`${styles.card_img}`}>
-                    <Image className={`${card_play}`} src={card_play} alt=""/>
-                </div>
-                <div className={`${styles.card_body}`}>
-                    <div className={`${styles.card_title}`}>@BlueWard2</div>
-                    <div className={`${styles.card_stats}`}>
-                        <div className={`${styles.stat_block_one}`}>
-                            <div className={`${styles.top}`}>
-                                <Image src={com} alt=""/>
-                                <h1 className={`${styles.users_amount}`}>500k</h1>
-                            </div>
-                            <div className={`${styles.members}`}>members</div>
-                        </div>
-                        <div className={`${styles.stat_block_two}`}>
-                            <div className={`${styles.text_one}`}>134 NMBL</div>
-                            <div className={`${styles.text_two}`}>floor price</div>
-                        </div>
-                    </div>
-                    <div className={`${styles.card_text}`}>
-                        Build an Evolving NFTs (Pokemon) Web3 
-                        App in 15 Minutes! | Custom smart 
-                        contract tutorial</div>
-                    <div className={`${styles.card_info}`}>
-                        <div className={`${styles.date}`}>
-                            <h2>Mint date</h2>
-                            <h1>01.01.23</h1>
-                        </div>
-                        <div className={`${styles.card_tags}`}>
-                            <div>#ETH-NFT</div>
-                            <div>#NFT-Trading</div>
-                            <div>#Discord-Tips</div>
-                        </div>
+    return <div className={`${styles.launchpad_card}`}>
+        <div className={`${styles.card_video}`}>
+            <Image className={`${styles.img}`} src={card_play} alt=""/>
+        </div>
+        <div className={`${styles.card_body}`}>
+            <div className={`${styles.card_title}`}><h1>@BlueWard2</h1></div>
+            <div className={`${styles.card_stat}`}>
+                <div>
+                    <div>
+                        <Sprite id="community"/>
                     </div>
                 </div>
-                <Image className={`${styles.card_corner_bottom}`} src={border_bottom} alt=""/>
-                <Image className={`${styles.card_corner_top}`} src={border_top} alt=""/>
+                <div></div>
             </div>
-  )
+            <div className={`${styles.card_text}`}></div>
+            <div className={`${styles.card_info}`}></div>
+        </div>
+    </div>
 }
 
-export default LaunchpadCard;
+export default LaunchpadCard
--- src/pages/marketplace/launchpad/index.tsx
+++ src/pages/marketplace/launchpad/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import Rankings from '@/components/Rankings/index'
+import LaunchpadCard from '@/components/LaunchpadCard/index'
+import styles from './Launchpad.module.scss'
 
 const LaunchpadPage = () => {
     return (
         <Layout>
-            <div>LaunchpadPage</div>
+            {/* <Rankings/> */}
+            <div className={`${styles.launchpad_wrapper}`}>
+                <div className={`${styles.launchpad_navigation}`}>
+                    <div className={`${styles.nav_item}`}></div>
+                    <div className={`${styles.nav_item}`}></div>
+                    <div className={`${styles.nav_item}`}></div>
+                </div>
+                <div className={`${styles.launchpad_cards}`}>
+                    <LaunchpadCard/>
+                </div>
+            </div>
         </Layout>
 
     )
